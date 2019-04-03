a67eabb408a33b768649fc5b31e2ba2493fbde8b
Akash <akashniki@gmail.com>
2019-04-03 18:50:23 +0200

    sdm845-common:Update clang to 9.0.2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := 9.0.1
+TARGET_KERNEL_CLANG_VERSION := 9.0.2
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
